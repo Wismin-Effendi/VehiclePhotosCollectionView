ddb21f3c15bb7ab5ae9f6849ccaf32d73dfebf7c
Wismin Effendi <wismin.effendi@gmail.com>
2017-06-03 11:16:42 -0500

    Add second section for Helicopter

--- VechiclePhotosCollectionView/VehicleCollectionViewController.swift
+++ VechiclePhotosCollectionView/VehicleCollectionViewController.swift
@@ -14,7 +14,7 @@ class VehicleCollectionViewController: UICollectionViewController {
     
     var carPhotos = [Photo]()
     var helicopterPhotos = [Photo]()
-    
+    var allPhotos = [[Photo]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,32 +25,40 @@ class VehicleCollectionViewController: UICollectionViewController {
 
         for i in 1...15 {
             if i > 9 {
-                carPhotos.append(Photo(title: "Title #\(i)", kind: .car, imageName: "images/car\(i).jpeg"))
+                carPhotos.append(Photo(title: "Car #\(i)", kind: .car, imageName: "images/car\(i).jpeg"))
             } else {
-                carPhotos.append(Photo(title: "Title #0\(i)", kind: .car, imageName: "images/car0\(i).jpeg"))
+                carPhotos.append(Photo(title: "Car #0\(i)", kind: .car, imageName: "images/car0\(i).jpeg"))
             }
         }
 
+        for i in 1...15 {
+            if i > 9 {
+                helicopterPhotos.append(Photo(title: "Helicopter #\(i)", kind: .car, imageName: "images/chop\(i).jpeg"))
+            } else {
+                helicopterPhotos.append(Photo(title: "Helicopter #0\(i)", kind: .car, imageName: "images/chop0\(i).jpeg"))
+            }
+        }
+        
+        allPhotos.append(carPhotos)
+        allPhotos.append(helicopterPhotos)
     }
-
-
     
     
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
+        return allPhotos.count
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return carPhotos.count
+        return allPhotos[section].count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! PhotoCell
     
-        let photo = carPhotos[indexPath.row]
+        let photo = allPhotos[indexPath.section][indexPath.row]
         cell.photo = photo
     
         return cell
@@ -60,7 +68,12 @@ class VehicleCollectionViewController: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         
         let sectionHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "SectionHeader", for: indexPath) as! PhotoHeader
-        let title = "Cars"
+        var title = ""
+        
+        if let kind = Kind(rawValue: indexPath.section) {
+            title = kind.description()
+        }
+        
         sectionHeader.title = title
         
         return sectionHeader
